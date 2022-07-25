562f2bc9ef86646344959425b050e7f646c5bf0c
nur <medo0o6665@gmail.com>
2022-07-26 03:42:28 +0800

    set up Home page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import GlobalStyle from "./app.styles";
+import { Home } from "./pages";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<div>home</div>} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<div>login</div>} />
       </Routes>
     </>
