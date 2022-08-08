2ce999f432bfd85543b7d63b0c9d1435b377d757
nur <medo0o6665@gmail.com>
2022-08-08 22:23:49 +0800

    make FeaturesList component more dynamic

--- frontend/src/components/featuresList/FeaturesList.jsx
+++ frontend/src/components/featuresList/FeaturesList.jsx
@@ -1,71 +1,34 @@
 import React from "react";
-import { getIconById, ICONS_ID } from "../../icons";
+import { getIconById } from "../../icons";
+import { featuresListData } from "./data";
 import { MainWrapper, SvgWrapper } from "./featuresList.styles";
 
 export const FeaturesList = () => {
   return (
     <MainWrapper>
       <ul class="features-list">
-        <li>
-          <a
-            href="#"
-            data-featherlight="#features-list-rewards"
-            data-featherlight-variant="fancy"
-          >
-            <span class="inner-icon">
-              <SvgWrapper childStyle="width:1em; height:1em; margin-bottom: -.21em; margin-left:.3em;">
-                {getIconById(ICONS_ID.IconMnsRewards)}
-              </SvgWrapper>
-              {/* <svg >
+        {featuresListData.map(
+          ({ title, iconID, content }, index) => (
+            <li key={`index-${index}`}>
+              <a
+                href="#"
+                data-featherlight="#features-list-rewards"
+                data-featherlight-variant="fancy"
+              >
+                <span class="inner-icon">
+                  <SvgWrapper childStyle="width:1em; height:1em; margin-bottom: -.21em; margin-left:.3em;">
+                    {getIconById(iconID)}
+                  </SvgWrapper>
+                  {/* <svg >
                   <use href="/store/skin/frontend/mnsv4/default/images/svg-icons/icons.svg#icon-mns-rewards"></use>
                 </svg> */}
-            </span>
-            Earn Rewards{" "}
-            <span class="new-line">On Everything</span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            data-featherlight="#features-list-samples"
-            data-featherlight-variant="fancy"
-          >
-            <span class="inner-icon">
-              <SvgWrapper childStyle="width:1em; height:1em; margin-bottom: -.15em; margin-left:.3em;">
-                {getIconById(ICONS_ID.IconDeal)}
-              </SvgWrapper>
-            </span>
-            Unbeatable <span class="new-line">Deals</span>
-          </a>
-        </li>
-        <li>
-          <a
-            href=""
-            class="modal-dialog same-day-shipping-block"
-            onclick="return false;"
-          >
-            <span class="inner-icon">
-              <SvgWrapper childStyle="width:1.1em; height:1.1em; margin-bottom: -.18em; margin-left:.3em;">
-                {getIconById(ICONS_ID.IconTruck)}
-              </SvgWrapper>
-            </span>
-            Same Day <span class="new-line">Shipping</span>{" "}
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            data-featherlight="#features-list-returns"
-            data-featherlight-variant="fancy"
-          >
-            <span class="inner-icon">
-              <SvgWrapper childStyle="width:1em; height:1em; margin-bottom: -.15em; margin-left:.3em;">
-                {getIconById(ICONS_ID.IconSavedCards)}
-              </SvgWrapper>
-            </span>
-            Money Back <span class="new-line">Guarantee</span>
-          </a>
-        </li>
+                </span>
+                {title[0]}{" "}
+                <span class="new-line">{title[1]}</span>
+              </a>
+            </li>
+          )
+        )}
       </ul>
     </MainWrapper>
   );
