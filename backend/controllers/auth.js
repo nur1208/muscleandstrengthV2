f931d1d702270bc938d1a5cacd9b6c7346e6130e
nur <medo0o6665@gmail.com>
2022-08-22 05:40:36 +0800

    create 'login' api route

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import UserModel from "../models/user.js";
+import AppError from "../utils/appError.js";
 import catchAsync from "../utils/catchAsync.js";
 const signToken = (id) => {
   const { JWT_SECRET, JWT_EXPIRES_IN } = process.env;
@@ -45,4 +46,39 @@ export const signUp = catchAsync(async (req, res) => {
   });
 
   createSendToken(newUser, 201, req, res);
+});
+
+export const login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1 check if email and password exist
+
+  if (!email || !password) {
+    return next(
+      new AppError("please provide email and password", 400)
+    );
+  }
+
+  // 2 check if a user exist and the password correct
+
+  // email = c@c.com
+  const user = await UserModel.findOne({ email }).select(
+    "+password"
+  );
+
+  const isCorrectPassword = await user?.correctPassword(
+    password,
+    user.password
+  );
+
+  // 401 means unauthorized
+  if (!user || !isCorrectPassword) {
+    return next(
+      new AppError("Incorrect email or password", 401)
+    );
+  }
+
+  // 3 if the everything okay send the token to the clint
+
+  createSendToken(user, 200, req, res);
 });
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signUp } from "../controllers/auth.js";
+import { login, signUp } from "../controllers/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", signUp);
+userRouter.post("/login", login);
 
 export default userRouter;
