9ab9cd70e8f2701f94968a4f96b2d991e5dc64da
nur <medo0o6665@gmail.com>
2022-08-31 12:10:41 +0800

    create PageTitle component

---
+++ frontend/src/components/pageTitle/PageTitle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MainWrapper } from "./pageTitle.styles";
+
+export const PageTitle = () => {
+  return (
+    <MainWrapper>
+      <h1 class="page-title center-align box-title grid-cell">
+        Search Results
+      </h1>
+    </MainWrapper>
+  );
+};
---
+++ frontend/src/components/pageTitle/pageTitle.styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const MainWrapper = styled.div`
+  text-align: center;
+  background-color: #fff;
+
+  margin-bottom: 10px;
+  padding-top: 20px;
+  font-weight: 600;
+  h1 {
+    font-size: 2em;
+    margin: 0.67em 0;
+  }
+  .align-center,
+  .center,
+  .center-align {
+    text-align: center;
+  }
+
+  h1.page-title {
+    font-size: 25px;
+    margin: 0 0 15px;
+  }
+
+  @media (min-width: 840px) {
+    margin-bottom: 15px;
+    padding-top: 25px;
+    h1.page-title {
+      margin: 0 0 15px;
+    }
+  } ;
+`;
