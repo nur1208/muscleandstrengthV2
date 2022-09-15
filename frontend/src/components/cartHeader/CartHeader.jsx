b7ca6e8958108168b1f881e6cf0f2a6c99ddcbf2
nur <medo0o6665@gmail.com>
2022-09-15 15:33:01 +0800

    make CartHeader component more dynamic

--- frontend/src/components/cartHeader/CartHeader.jsx
+++ frontend/src/components/cartHeader/CartHeader.jsx
@@ -5,19 +5,25 @@ import { getIconById, ICONS_ID } from "../../icons";
 import { Button } from "../button/Button";
 import { MainWrapper } from "./cartHeader.styles";
 
-export const CartHeader = () => {
+export const CartHeader = ({ text }) => {
   const isPhone = useMediaQuery({ maxWidth: 480 });
 
   const navigate = useNavigate();
   const handleContinueShopping = () => {
     navigate("/store");
   };
+
+  const handleCheckout = () => {
+    navigate("/store/checkout/onpage");
+  };
   return (
     <MainWrapper>
-      <div class="cartHeader">
-        <h1 class=" cart-title">
-          <span class="cart-header-text">Shopping Cart</span>
-          <div class="shopper badge">
+      <div className="cartHeader">
+        <h1 className=" cart-title">
+          <span className="cart-header-text">
+            {text || "Shopping Cart"}
+          </span>
+          <div className="shopper badge">
             <a
               href="http://shopperapprovedreviews.com/reviews/muscleandstrength.com/"
               onclick="var nonwin=navigator.appName!='Microsoft Internet Explorer'?'yes':'no'; var certheight=screen.availHeight-90; window.open(this.href,'shopperapproved','location='+nonwin+',scrollbars=yes,width=620,height='+certheight+',menubar=no,toolbar=no'); return false;"
@@ -29,7 +35,7 @@ export const CartHeader = () => {
               />
             </a>
           </div>
-          <div class="bbb badge">
+          <div className="bbb badge">
             <a
               href="https://www.bbb.org/columbia/business-reviews/vitamins-and-food-supplements/muscle-and-strength-in-columbia-sc-34013064"
               target="_blank"
@@ -41,54 +47,63 @@ export const CartHeader = () => {
             </a>
           </div>
         </h1>
-        <div class="cart-feature-wrap">
-          <div class="feature-five">
-            {getIconById(ICONS_ID.IconLowPrice)}
+        {!text && (
+          <>
+            <div className="cart-feature-wrap">
+              <div className="feature-five">
+                {getIconById(ICONS_ID.IconLowPrice)}
 
-            <span class="text">Lowest Prices</span>
-          </div>
-          <div class="feature-one">
-            {getIconById(ICONS_ID.IconDeal, null, "#AAA")}
-            <span class="text">Best Deals</span>
-          </div>
-          <div class="feature-three">
-            {getIconById(ICONS_ID.IconSale)}
-            <span class="text">Money Back Guarantee</span>
-          </div>
-          <div class="feature-four">
-            {getIconById(ICONS_ID.IconTruck, null, "#AAA")}
+                <span className="text">Lowest Prices</span>
+              </div>
+              <div className="feature-one">
+                {getIconById(ICONS_ID.IconDeal, null, "#AAA")}
+                <span className="text">Best Deals</span>
+              </div>
+              <div className="feature-three">
+                {getIconById(ICONS_ID.IconSale)}
+                <span className="text">
+                  Money Back Guarantee
+                </span>
+              </div>
+              <div className="feature-four">
+                {getIconById(ICONS_ID.IconTruck, null, "#AAA")}
 
-            <span class="text">Super Fast Shipping</span>
-          </div>
-          <div class="feature-two">
-            {getIconById(
-              ICONS_ID.IconSupportWhite,
-              null,
-              "#AAA"
-            )}
-            <span class="text">7 Day Support</span>
-          </div>
-        </div>
-        <div class="button-row">
-          <Button
-            text="Continue Shopping"
-            isSmall
-            onClick={handleContinueShopping}
-          />
-          {/* <button
+                <span className="text">Super Fast Shipping</span>
+              </div>
+              <div className="feature-two">
+                {getIconById(
+                  ICONS_ID.IconSupportWhite,
+                  null,
+                  "#AAA"
+                )}
+                <span className="text">7 Day Support</span>
+              </div>
+            </div>
+            <div className="button-row">
+              <Button
+                text="Continue Shopping"
+                isSmall
+                onClick={handleContinueShopping}
+              />
+              {/* <button
             type="button"
-            class="btn btn-white btn-sm btn-auto btn-shop"
+            className="btn btn-white btn-sm btn-auto btn-shop"
             onclick="setLocation('https://www.muscleandstrength.com/store/')"
           >
             Continue Shopping
           </button> */}
-          <div class="grid-spacer"></div>
-          <Button
-            text={isPhone ? "Checkout" : "Proceed to Checkout"}
-            isGreen
-            isLarge
-          />
-        </div>
+              <div className="grid-spacer"></div>
+              <Button
+                text={
+                  isPhone ? "Checkout" : "Proceed to Checkout"
+                }
+                isGreen
+                onClick={handleCheckout}
+                isLarge
+              />
+            </div>
+          </>
+        )}
       </div>
     </MainWrapper>
   );
