128bf5588683735811ffd487039ae4c28a06e234
nur <medo0o6665@gmail.com>
2022-09-27 11:39:47 +0800

    make  MainPageCategory component more dynamic

--- frontend/src/components/mainPageCategory/MainPageCategory.jsx
+++ frontend/src/components/mainPageCategory/MainPageCategory.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { GridX } from "../../app.styles";
+import { category } from "./data";
 import { MainWrapper } from "./mainPageCategory.styles";
 
 export const MainPageCategory = () => {
@@ -7,27 +8,23 @@ export const MainPageCategory = () => {
     <MainWrapper>
       <GridX>
         <div class="grid-x grid-margin-x grid-margin-y small-up-2 bp600-up-3 medium-up-4">
-          {Array(20)
-            .fill(true)
-            .map(() => (
-              <div class="cell">
-                <a href="/workouts/men">
-                  <img
-                    data-src="https://cdn.muscleandstrength.com/sites/default/files/styles/400x250/public/taxonomy/image/workouts/workoutsformen.jpg?itok=jALf6fyP"
-                    class=" ls-is-cached lazyloaded"
-                    width="400"
-                    height="250"
-                    alt="Lean muscular man doing bicep curls in the gym"
-                    src="https://cdn.muscleandstrength.com/sites/default/files/styles/400x250/public/taxonomy/image/workouts/workoutsformen.jpg?itok=jALf6fyP"
-                  />
-                </a>
-                <a href="/workouts/men">
-                  <div class="category-name">
-                    Workouts For Men
-                  </div>
-                </a>
-              </div>
-            ))}
+          {category.map(({ href, imgUrl, categoryName }) => (
+            <div class="cell">
+              <a href="/workouts/men">
+                <img
+                  data-src="https://cdn.muscleandstrength.com/sites/default/files/styles/400x250/public/taxonomy/image/workouts/workoutsformen.jpg?itok=jALf6fyP"
+                  class=" ls-is-cached lazyloaded"
+                  width="400"
+                  height="250"
+                  alt={categoryName}
+                  src={imgUrl}
+                />
+              </a>
+              <a href={href}>
+                <div class="category-name">{categoryName}</div>
+              </a>
+            </div>
+          ))}
         </div>
       </GridX>
     </MainWrapper>
