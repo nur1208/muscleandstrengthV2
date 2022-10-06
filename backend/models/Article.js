9ea618971e51e81d0c1477018ec5bdd576896efd
nur <medo0o6665@gmail.com>
2022-10-06 23:14:52 +0800

    update Article model in the backend

--- backend/models/Article.js
+++ backend/models/Article.js
@@ -2,18 +2,20 @@ import mongoose from "mongoose";
 
 const schema = new mongoose.Schema(
   {
-    href: String,
-    imgUrl: String,
+    sourceUrl: { type: String, unique: true },
+    imgUrl: [String],
     imgAlt: String,
     title: String,
     shortSummary: String,
     reads: Number,
     comments: Number,
+    category: [String],
     type: String,
     isWorkout: Boolean,
     level: String,
     forGender: String,
     time: String,
+    body: String,
   },
   {
     timestamps: true,
