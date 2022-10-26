7c2fad8f48270130ca64436d8223d23043cb91ab
nur <medo0o6665@gmail.com>
2022-10-26 16:43:08 +0800

    change API url in the frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API = (userToken) => {
   return axios.create({
-    baseURL: `http://localhost:4050/api`,
-    // baseURL: `https://muscleandstrength-v2-backend-hedecw44s-nur1208.vercel.app/api`,
+    // baseURL: `http://localhost:4050/api`,
+    baseURL: `https://muscleandstrength-v2-backend.vercel.app/api`,
 
     headers: userToken
       ? { authorization: `Bearer ${userToken}` }
